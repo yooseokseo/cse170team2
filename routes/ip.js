54974ad260e000748e6e73301ae25f16f6ac87fb
Peem Takoonsawat <mrpeem@gmail.com>
2018-03-03 16:16:39 -0800

    get machine name (hostname) in addition to IP

--- routes/ip.js
+++ routes/ip.js
@@ -2,48 +2,57 @@ var http = require('http');
 var userIP = require('../userIP.json');
 var userData = require('../userData.json');
 var defaultUserData = require('../defaultUserData.json');
-var ipAddress, ipIndex;
-
-
-http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) 
-{
-  resp.on('data', function(ip) 
-  {
-    console.log("ip = "+ip);
-    ipAddress = ip;
-  });
-});
-
-exports.getIP = function()
-{
-  return ipAddress;
-}
+var ipIndex, hostname;
 
 exports.getIPIndex = function()
 {
   return ipIndex;
 }
 
-exports.getUserData = function()
-{
+
+ 
+function getIP() 
+{ 
+  return new Promise(function(resolve, reject) 
+  {
+    http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) 
+    {
+      resp.on('data', function(ip) 
+      {
+        hostname = require('os').hostname();
+        resolve(ip.toString());
+      });
+    });
+  });
+}
+
+exports.getUserData = async function main() {
+  var ip = await getIP();
+
   var i, ipIndex;
   for (i = 0; i < userIP.length; i++)
   {
-    if (ipAddress == userIP[i].ip)
+    if (ip == userIP[i].ip &&
+        hostname == userIP[i].hostname)
     {
       console.log("user ip in system");
       ipIndex = i;
       return userData[ipIndex];
     }
   }
+  console.log("user not in system yet; push to userIP.json");
 
   ipIndex = userIP.length; 
-  userIP.push( {"ip": ipAddress} ); //store IP
+  userIP.push( {"ip": ip, "hostname" : hostname} ); //store IP
+  console.log(userIP[ipIndex]);
+
   var newUserData = JSON.parse(JSON.stringify(defaultUserData));
+  newUserData.ipIndex = ipIndex;
+  newUserData.ip = ip;
+  newUserData.hostname = hostname;
 
   userData.push(newUserData);      //add new userData to the array
-  userData[ipIndex].ipIndex = ipIndex;
-
+  
 
   return userData[ipIndex];
 
